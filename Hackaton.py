f333b78d3e5cc218dc11029c5ab44073d2071d23
adriannrdz <102832144+adriannrdz@users.noreply.github.com>
2022-05-01 12:26:03 -0600

    Update Hackaton.py

--- Hackaton.py
+++ Hackaton.py
@@ -93,6 +93,27 @@ st.markdown("""
                     </style>
                     """, unsafe_allow_html=True)
 
+st.markdown(
+            """
+            <style>
+    @font-face {
+      font-family: 'Roboto Slab', serif;
+      font-style: normal;
+      font-weight: 400;
+      src: url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+      unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+
+        html, body, [class*="css"]  {
+        font-family: 'Roboto Slab';
+        font-size: 30px;
+        }
+        </style>
+
+        """,
+            unsafe_allow_html=True,
+        )
+
 with mt1:
     st.text("")
 
@@ -107,7 +128,7 @@ with mt6:
     st.text("")
     
 with titlep:
-    st.markdown('<p class="big-font"> Obtencion de Reacciones Quimicas. </p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="font-face">Obtencion de Reacciones Quimicas</p>', unsafe_allow_html=True)
     
 with mt7:
     st.text("")   
