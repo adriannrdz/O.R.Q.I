d94ee319e0fbaea14518a2fdb3fd17c038c690af
adriannrdz <102832144+adriannrdz@users.noreply.github.com>
2022-05-01 12:44:24 -0600

    Update Hackaton.py

--- Hackaton.py
+++ Hackaton.py
@@ -97,16 +97,16 @@ st.markdown(
             """
             <style>
     @font-face {
-      font-family: 'Roboto Slab', serif;
+      font-family:'Montserrat Alternates', sans-serif;
       font-style: normal;
       font-weight: 400;
-      src: url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+      src: url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@300&display=swap');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
 
         html, body, [class*="css"]  {
-        font-family: 'Roboto Slab';
-        font-size: 15px;
+        font-family: 'Montserrat Alternates', sans-serif;
+        font-size: 20px;
         }
         </style>
 
